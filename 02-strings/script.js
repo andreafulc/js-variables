7a62b694e1a88fe651d929f9ff7475a9e3bc992d
fulciniti.andre@gmail.com <fulciniti.andre@gmail.com>
2025-02-15 09:34:17 +0100

    fix: Myname on 2.1

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,8 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name = "Andrea";  
-console.log(name);
+const myName = "Andrea";  
+console.log(myName);
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
